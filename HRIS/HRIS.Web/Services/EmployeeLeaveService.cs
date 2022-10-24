2e825ad00c6a00eff88cee2ae9c452582c3d5f48
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-24 19:20:09 +0600

    EmployeeLeave Service Implemented

---
+++ HRIS/HRIS.Web/Services/EmployeeLeaveService.cs
@@ -0,0 +1,24 @@
+﻿using HRIS.Web.Models;
+using HRIS.Web.Repository;
+
+namespace HRIS.Web.Services
+{
+    public class EmployeeLeaveService : IEmployeeLeaveService
+    {
+        public EmployeeLeaveService()
+        {
+
+        }
+
+        public void CreateLeaveType(EmployeeLeaveModel employeeLeave, IUnitOfWork unitOfWork)
+        {
+            unitOfWork.EmployeeLeave.Add(employeeLeave);
+            unitOfWork.Save();
+        }
+
+        public IEnumerable<EmployeeLeaveModel> GetAll(IUnitOfWork unitOfWork)
+        {
+            return unitOfWork.EmployeeLeave.GetAll();
+        }
+    }
+}
---
+++ HRIS/HRIS.Web/Services/IEmployeeLeaveService.cs
@@ -0,0 +1,11 @@
+﻿using HRIS.Web.Models;
+using HRIS.Web.Repository;
+
+namespace HRIS.Web.Services
+{
+    public interface IEmployeeLeaveService
+    {
+        void CreateLeaveType(EmployeeLeaveModel employeeLeave, IUnitOfWork unitOfWork);
+        IEnumerable<EmployeeLeaveModel> GetAll(IUnitOfWork unitOfWork);
+    }
+}
