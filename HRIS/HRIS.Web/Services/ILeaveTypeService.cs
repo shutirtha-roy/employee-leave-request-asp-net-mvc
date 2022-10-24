d29b5e350213a5c148fe79b312a160d59f3056e4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-24 18:56:37 +0600

    LeaveTypeService Implemented

---
+++ HRIS/HRIS.Web/Services/ILeaveTypeService.cs
@@ -0,0 +1,12 @@
+﻿using HRIS.Web.Models;
+using HRIS.Web.Repository;
+
+namespace HRIS.Web.Services
+{
+    public interface ILeaveTypeService
+    {
+        void CreateLeaveType(LeaveTypeModel leaveType, IUnitOfWork unitOfWork);
+        IEnumerable<LeaveTypeModel> GetAll(IUnitOfWork unitOfWork);
+        dynamic GetData(IUnitOfWork unitOfWork);
+    }
+}
---
+++ HRIS/HRIS.Web/Services/LeaveTypeService.cs
@@ -0,0 +1,31 @@
+﻿using HRIS.Web.Models;
+using HRIS.Web.Repository;
+
+namespace HRIS.Web.Services
+{
+    public class LeaveTypeService : ILeaveTypeService
+    {
+        
+        public LeaveTypeService()
+        {
+            
+        }
+
+        public void CreateLeaveType(LeaveTypeModel leaveType, IUnitOfWork unitOfWork)
+        {
+            unitOfWork.LeaveType.Add(leaveType);
+            unitOfWork.Save();
+        }
+
+        public IEnumerable<LeaveTypeModel> GetAll(IUnitOfWork unitOfWork)
+        {
+            return unitOfWork.LeaveType.GetAll();
+        }
+
+        public dynamic GetData(IUnitOfWork unitOfWork)
+        {
+            var enumerator = from value in unitOfWork.LeaveType.GetAll() select new { value.Id, value.Title };
+            return enumerator;
+        }
+    }
+}
