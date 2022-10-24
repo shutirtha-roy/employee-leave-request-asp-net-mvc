2a03abfabaa54b9fc493c8d71a236ac3b33eeffe
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-24 17:46:14 +0600

    EmployeeLeave Initial Commit

---
+++ HRIS/HRIS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HRIS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
