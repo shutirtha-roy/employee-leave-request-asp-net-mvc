2cf9a865ddec0c3ea38570442a1ac1ab791a9ded
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-24 18:56:18 +0600

    LeaveType Migration Added

---
+++ HRIS/HRIS.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HRIS.Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRIS.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveTypeModel> LeaveTypes { get; set; }
+        public DbSet<EmployeeLeaveModel> EmployeeLeaves { get; set; }
+    }
+}
---
+++ HRIS/HRIS.Web/Data/Migrations/20221023190419_AddLeaveTypeTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRIS.Web.Data.Migrations
+{
+    public partial class AddLeaveTypeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeaveTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveTypes");
+        }
+    }
+}
